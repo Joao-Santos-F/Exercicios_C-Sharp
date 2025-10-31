cfeb896c764ca4ec634f76127f54ba1845a3b647
Joao <joaofeitosa029@gmail.com>
2025-10-31 09:18:55 -0300

    Feito aos exercícios com classe

---
+++ POO/PrimeiraClasse/Carro.cs
@@ -0,0 +1,23 @@
+namespace PrimeiraClasse
+{
+    public class Carro
+    {
+        public string Modelo;
+        public string Marca;
+        public string Cor;
+        public int Potencia;
+
+        public void ligar()
+        {
+            Console.WriteLine($"zizizi... vrum");
+
+        }
+
+        public void Acelerar()
+        {
+            Console.WriteLine($"Vrum, Vrum...");
+
+        }
+    }
+
+}
